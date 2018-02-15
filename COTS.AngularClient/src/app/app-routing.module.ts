9ed393c2ad87e394a62293220300cb18101560fa
jGold6001 <jgold6001@gmail.com>
2018-02-15 23:48:48 +0200

    halls schemes made

--- COTS.AngularClient/src/app/app-routing.module.ts
+++ COTS.AngularClient/src/app/app-routing.module.ts
@@ -6,12 +6,18 @@ import { MoviePageComponent } from "./components/movie-page.components/movie-pag
 import { PurchasePageComponent } from "./components/purchase-page.components/purchase-page/purchase-page.component";
 import { HallDialogComponent } from "./components/hall-dialog.components/hall-dialog/hall-dialog.component";
 import { HallFourthComponent } from "./components/hall-dialog.components/halls-components/hall-fourth/hall-fourth.component";
+import { HallThirdComponent } from "./components/hall-dialog.components/halls-components/hall-third/hall-third.component";
+import { HallSecondComponent } from "./components/hall-dialog.components/halls-components/hall-second/hall-second.component";
+import { HallFirstComponent } from "./components/hall-dialog.components/halls-components/hall-first/hall-first.component";
 
 
 
 const routes: Routes = [
   { path: "", component: MainPageComponent }, 
-  { path: "hall", component: HallFourthComponent },
+  { path: "hall_4", component: HallFourthComponent },
+  { path: "hall_3", component: HallThirdComponent },
+  { path: "hall_2", component: HallSecondComponent },
+  { path: "hall_1", component: HallFirstComponent },
   { path: "purchase/:id", component: PurchasePageComponent},
   { path: ":cityId", component: MainPageComponent },
   { path: ":cityId/movie/:id", component: MoviePageComponent},
